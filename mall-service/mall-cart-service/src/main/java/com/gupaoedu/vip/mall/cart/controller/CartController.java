e25cf3a30c9766a87c23fd2caea445b2f316699f
殷建军 <yinjianjun@shinhofood.com>
2021-09-01 18:23:51 +0800

    seata

--- mall-service/mall-cart-service/src/main/java/com/gupaoedu/vip/mall/cart/controller/CartController.java
+++ mall-service/mall-cart-service/src/main/java/com/gupaoedu/vip/mall/cart/controller/CartController.java
@@ -20,6 +20,8 @@ public class CartController {
     /***
      * 添加购物车
      * id: skuid
+     * http://localhost:8087/cart/1318596430360813570/1
+     *
      */
     @GetMapping(value = "/{id}/{num}")
     public RespResult add(@PathVariable(value = "id")String id, @PathVariable(value = "num")Integer num){
@@ -48,6 +50,9 @@ public class CartController {
     public RespResult<List<Cart>> list(@RequestBody List<String> ids){
         //购物车集合
         List<Cart> carts = Lists.newArrayList(cartService.list(ids));
+//        String userName = "gp";
+//        List<Cart> carts = cartService.list(userName);
+
         return RespResult.ok(carts);
     }
 
--- mall-service/mall-order-service/src/main/java/com/gupaoedu/vip/mall/order/controller/OrderController.java
+++ mall-service/mall-order-service/src/main/java/com/gupaoedu/vip/mall/order/controller/OrderController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /*****
  * @Author:
@@ -26,10 +28,26 @@ public class OrderController {
 
     /***
      * 添加订单
+     *http://localhost:8089/order/add
+     *    {
+     *     "payType":"1",
+     *     "username":"testname",
+     *     "recipients":"testname",
+     *     "recipientsMobile":139,
+     *     "recipientsAddress":"testaddress",
+     *     "weixinTransactionId":1,
+     *     "totalNum":15,
+     *     "moneys":5,
+     *     "orderStatus":1,
+     *     "payStatus":1,
+     *     "cartIds":[1318596430360813570],
+     *     "payTime":"2012-01-01",
+     *      "consignTime":"2021-01-01",
+     *       "endTime":"2021-01-01"
      *
-     *
+     *          }
      */
-    @PostMapping
+    @PostMapping(value = "/add")
     public RespResult add(@RequestBody Order order, HttpServletRequest request) throws Exception {
         String userName = "gp";
         order.setUsername(userName);
@@ -39,6 +57,9 @@ public class OrderController {
         order.setOrderStatus(0);
         order.setPayStatus(0);
         order.setIsDelete(0);
+        List<String> list=new ArrayList<>();
+        list.add("gp1318596430360813570");
+        order.setCartIds(list);
 
         //添加订单
         Boolean bo = orderService.add(order);
